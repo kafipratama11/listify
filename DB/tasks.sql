f7b2d29f46b433634f30c67c588bd0e40f359151
kafipratama11 <kafipratama1512@gmail.com>
2025-04-22 15:23:08 +0700

    database added

---
+++ DB/tasks.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 22, 2025 at 08:20 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.2.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_todolist_ukk_kafi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int NOT NULL,
+  `id_user` int NOT NULL,
+  `id_category` int NOT NULL,
+  `id_status` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `task_date` date NOT NULL,
+  `deadline` date DEFAULT NULL,
+  `archive_status` enum('active','archived') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `id_user`, `id_category`, `id_status`, `title`, `description`, `task_date`, `deadline`, `archive_status`, `created_at`, `updated_at`) VALUES
+(9, 1, 2, 3, 'dh dhdhdhdggg 777', 'd hdhdh yyyyy', '2025-04-25', '2025-04-26', 'active', '2025-04-21 21:33:42', '2025-04-21 23:28:39'),
+(11, 1, 2, 1, 'xvxgfxgf', 'gf gx x', '2025-04-24', '2025-04-25', 'active', '2025-04-21 21:40:36', '2025-04-21 23:48:56'),
+(12, 1, 1, 2, 'vfddg dgdg', 'dgd gdgd', '2025-04-24', NULL, 'active', '2025-04-21 21:45:40', '2025-04-21 23:49:00'),
+(13, 1, 3, 1, 'fs fsfsfsf', 'sf sfsfsfs', '2025-04-25', NULL, 'archived', '2025-04-21 23:33:49', '2025-04-21 23:43:32'),
+(14, 2, 2, 2, 'task 1', 'desc task 1', '2025-04-23', '2025-04-24', 'active', '2025-04-21 23:56:32', '2025-04-21 23:59:50'),
+(15, 2, 1, 1, 'task 2', 'desc task 2', '2025-04-23', NULL, 'active', '2025-04-21 23:57:01', '2025-04-21 23:57:01');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
